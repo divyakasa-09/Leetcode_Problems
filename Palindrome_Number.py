d474a4f11d270d7872b1378c9242b52ca84d54b5
Divya Kasa <divyakasa@Divyas-MacBook-Pro.local>
2023-10-02 14:13:35 -0400

    Palindrome_Number

---
+++ Palindrome_Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False  # Negative numbers cannot be palindromes
+        
+        original_x = x
+        reversed_x = 0
+        
+        while x > 0:
+            digit = x % 10
+            reversed_x = reversed_x * 10 + digit
+            x //= 10
+        
+        return original_x == reversed_x
